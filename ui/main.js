27bf28145f43c1c1fc2925a17e8d654814f606d6
princeakshay27 <princeakshay27@gmail.com>
2017-02-11 00:04:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,11 +8,10 @@ var marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';//concanating px with 10
+    img.style.marginLeft=marginLeft+'px';
     
 }
 img.onclick=function()
 {
     var interval=setInterval(moveRight,50);
-    img.style.marginLeft='50px';
 };
