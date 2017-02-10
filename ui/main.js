7b341eae302cb094d4431dbc2256c5d8ac40c8d6
princeakshay27 <princeakshay27@gmail.com>
2017-02-11 00:27:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,7 @@
-console.log('Loaded!');
-//to change the inner text 
-var element= document.getElementById('main-text');
-element.innerHTML='new value';
-//to move the image
-var img = document.getElementById('pic');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-    
+//counter code
+var button=document.getElementById('counter');
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('counter');
+    span.innerHTML=counter.tostring();
 }
-img.onclick=function()
-{
-    var interval=setInterval(moveRight,50);
-};
