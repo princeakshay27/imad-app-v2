c37e896d6a52c7bb03a3e145c00c0b6ba78a7c1d
princeakshay27 <princeakshay27@gmail.com>
2017-02-10 23:31:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,15 @@ var element= document.getElementById('main-text');
 element.innerHTML='new value';
 //to move the image
 var img = document.getElementById('pic');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';//concanating px with 10
+    
+}
 img.onclick=function()
 {
+    var interval=setInterval(moveRight,100);
     img.style.marginLeft='100px';
 };
