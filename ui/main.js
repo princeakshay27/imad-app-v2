dc5eeee4baf88568c28c7162fd3475456401449e
princeakshay27 <princeakshay27@gmail.com>
2017-02-10 23:19:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
+//to change the inner text 
 var element= document.getElementById('main-text');
 element.innerHTML='new value';
+//to move the image
+var img = documentgetElementById('img');
+img.onclick=function()
+{
+    img.style.marginleft='100px';
+};
